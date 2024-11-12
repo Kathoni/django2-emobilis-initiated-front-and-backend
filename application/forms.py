aac6f9276a3c62397f7c02f9364a572f342ccd14
Kathoni <thrusherkeys@gmail.com>
2024-11-12 15:05:45 +0300

    image addition

--- application/forms.py
+++ application/forms.py
@@ -9,8 +9,13 @@ class StudentForm(forms.ModelForm):
         fields = '__all__'
 
         widgets = {
+
             'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your name'}),
             'age': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter your age'}),
             'gender': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your gender'}),
             'course': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your course'}),
+            'image': forms.ClearableFileInput(attrs={
+                'class': 'form-control',
+                'accept':'image/*',
+                'title':'Upload image here'})
         }
--- application/models.py
+++ application/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Student (models.Model):
+    image = models.ImageField(upload_to='student_images/', blank=True)
     name = models.CharField(max_length=20)
     age = models.IntegerField()
     gender = models.CharField(max_length=10)
